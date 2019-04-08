70e3d0e6872acfb7df1ccb54ad0675a7c2768d49
ervin <ervin.duttoo@leikir.io>
2019-04-08 15:01:54 +0200

    formation/13 refacto test

--- spec/transactions/user_spec.rb
+++ spec/transactions/user_spec.rb
@@ -1,29 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe CreateUser do
-  context 'create user' do
-    let(:random_user) { attributes_for(:random_user) }
-    it 'should create user' do
-      create_user = CreateUser.new
-      expect(create_user.call(user)).to be_success
+  describe '#user' do
+    subject { described_class.call(params: params) }
+
+    context 'when params are valid' do
+      let(:random_user) { attributes_for(:random_user) }
+      it 'should create user' do
+        create_user = CreateUser.new
+        expect(create_user.call(random_user)).to be_success
+      end
+
+      it 'should create user and save to database' do
+        create_user = CreateUser.new
+        expect { create_user.call(random_user) }.to change(User, :count).by(+1)
+      end
     end
 
-    let(:random_user) { attributes_for(:random_user) }
-    it 'should create user and save to database' do
-      create_user = CreateUser.new
-      expect { create_user.call(user) }.to change(User, :count).by(+1)
+    context 'given params bad email format' do
+      let(:random_user) { attributes_for(:random_user, email: '111') }
+      it 'shout not be valid with wrong email' do
+        create_user = CreateUser.new
+        expect(create_user.call(random_user)).to be_failure
+      end
     end
 
-    let(:random_user) { attributes_for(:random_user, email: '111') }
-    it 'shout not be valid with wrong email' do
-      create_user = CreateUser.new
-      expect(create_user.call(random_user)).to be_failure
-    end
-
-    let(:random_user) { attributes_for(:random_user, first_name: '') }
-    it 'should not be valid with missing parameters' do
-      create_user = CreateUser.new
-      expect(create_user.call(random_user)).to be_failure
+    context 'given missing parameters' do
+      let(:random_user) { attributes_for(:random_user, first_name: '') }
+      it 'should not be valid with missing parameters' do
+        create_user = CreateUser.new
+        expect(create_user.call(random_user)).to be_failure
+      end
     end
   end
 end
