eec8341d9874ebd7bba5b81d2cc395ae93143b96
ervin <ervin.duttoo@leikir.io>
2019-04-09 16:40:36 +0200

    formation/20 add log as finished

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,5 +1,6 @@
 ActiveAdmin.register User do
-  permit_params :email, :password, :password_confirmation, :first_name, :last_name
+  permit_params :email, :password, :password_confirmation,
+                :first_name, :last_name, :date_of_birth
 
   index do
     selectable_column
@@ -23,10 +24,39 @@ ActiveAdmin.register User do
 
   form do |f|
     f.inputs do
-      f.input :email
-      f.input :first_name
-      f.input :last_name
+      if !f.object.new_record?
+        f.input :email
+        f.input :first_name
+        f.input :last_name
+      else
+        f.input :email
+        f.input :first_name
+        f.input :last_name
+        f.input :date_of_birth
+        f.input :password
+        f.input :password_confirmation
+      end
     end
     f.actions
+  end
+
+  show do |user|
+    attributes_table do
+      #We want to keep the existing columns
+      User.column_names.each do |column|
+        row column
+      end
+      #This is where we add a new column
+      row :login_as do
+        link_to "#{user.first_name}", login_as_admin_user_path(user), :target => '_blank'
+      end
+    end
+  end
+
+  # Allows admins to login as a user
+  member_action :login_as, :method => :get do
+    user = User.find(params[:id])
+    bypass_sign_in user
+    redirect_to user_session_path
   end
 end
