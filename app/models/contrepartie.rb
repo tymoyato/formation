5f770dc7cd74ab89ace87358137edf6a68d5608c
ervin <ervin.duttoo@leikir.io>
2019-04-11 18:18:51 +0200

    formation/26 correction models

--- app/models/contrepartie.rb
+++ app/models/contrepartie.rb
@@ -1,3 +1,3 @@
 class Contrepartie < ApplicationRecord
-  belongs_to :contributions
+  belongs_to :contribution
 end
--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -1,4 +1,4 @@
 class Contribution < ApplicationRecord
-  belongs_to :users
-  belongs_to :projects
+  belongs_to :user
+  belongs_to :project
 end
