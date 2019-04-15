a5ef779c1b3a6d19ae59de11499b6850ea4f04cd
ervin <ervin.duttoo@leikir.io>
2019-04-15 15:06:39 +0200

    formation/26 add recto pr

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -24,18 +24,11 @@ ActiveAdmin.register User do
 
   form do |f|
     f.inputs do
-      if !f.object.new_record?
+      if f.object.new_record?
         f.input :email
-        f.input :first_name
-        f.input :last_name
-      else
-        f.input :email
-        f.input :first_name
-        f.input :last_name
-        f.input :date_of_birth
-        f.input :password
-        f.input :password_confirmation
       end
+      f.input :password
+      f.input :password_confirmation
     end
     f.actions
   end
